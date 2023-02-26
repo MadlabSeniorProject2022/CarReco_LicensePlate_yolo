544021ce40c4e41ff65337042d1d8eaf5cfcfed2
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2023-02-26 15:02:29 +0700

    fix: app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flow import run_flow
 import os 
 import time
 import io
-
-from PIL import Image
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
@@ -17,13 +17,17 @@ def predict ():
     if request.files.get("image"):
         im_file = request.files["image"]
         im_bytes = im_file.read()
+        isExist = os.path.exists("./to_predict")
+        if not isExist:
+            # Create a new directory because it does not exist
+            os.makedirs("./to_predict")
         path = f"./to_predict/{int(time.time() * 1000000)}.jpg"
-        im_file = request.files["image"]
-        im_bytes = im_file.read()
-        im = Image.open(io.BytesIO(im_bytes))
-        plate_num, url = run_flow(im)
+        file_bytes = np.asarray(bytearray(io.BytesIO(im_bytes).read()), dtype=np.uint8)
+        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        cv2.imwrite(path, img)
+        plate_num, url = run_flow(path)
 
     return jsonify({'msg': 'success', 'plate_num': plate_num, 'plate_url': url})
-
+ 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
